4725756533548a5ca6631fe53a88eba337311184
Kevin <hellokevinvogel@gmail.com>
2022-03-25 15:32:28 +0100

    chore(): add main

---
+++ cmd/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/hellokvn/go-api-medium/pkg/books"
+	"github.com/hellokvn/go-api-medium/pkg/common/db"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	viper.SetConfigFile("./pkg/common/envs/.env")
+	viper.ReadInConfig()
+
+	port := viper.Get("PORT").(string)
+	dbUrl := viper.Get("DB_URL").(string)
+
+	r := gin.Default()
+	h := db.Init(dbUrl)
+
+	books.RegisterRoutes(r, h)
+	// register more routes here
+
+	r.Run(port)
+}
